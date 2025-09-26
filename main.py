471d7fdf1a10513f8b58661d211f435d83b96ad9
dawid.spalek <spalek.dawid@gmail.com>
2025-09-26 21:53:00 +0200

    add message list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,9 +11,13 @@ client = genai.Client(api_key=api_key)
 
 prompt = sys.argv[1]
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt,
+    contents=messages,
 )
 print(response.text)
 print(
